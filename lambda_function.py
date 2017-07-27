c229abe357276298cb5a0788f423d51147700ea5
xli <xli@yelp.com>
2017-07-27 16:53:19 -0700

    build multipe response cards

--- lambda_function.py
+++ lambda_function.py
@@ -21,15 +21,15 @@ logger.setLevel(logging.DEBUG)
 """ --- Helpers to build responses which match the structure of the necessary dialog actions --- """
 
 
-def build_response_card(title, subtitle, options):
+def build_response_card(title, subtitle, img_url, link_url, options=None):
     """
     Build a responseCard with a title, subtitle, and an optional set of options which should be displayed as buttons.
     """
-    buttons = None
-    if options is not None:
-        buttons = []
-        for i in range(min(5, len(options))):
-            buttons.append(options[i])
+    # buttons = None
+    # if options is not None:
+    #     buttons = []
+    #     for i in range(min(5, len(options))):
+    #         buttons.append(options[i])
 
     return {
         'contentType': 'application/vnd.amazonaws.card.generic',
@@ -37,9 +37,47 @@ def build_response_card(title, subtitle, options):
         'genericAttachments': [{
             'title': title,
             'subTitle': subtitle,
-            'buttons': buttons
+            'imageUrl': img_url,
+            'link_url': link_url,
+            # 'buttons': buttons,
         }]
     }
+
+
+def build_response_card_attachment(title, subtitle, img_url, link_url, options=None):
+    """
+    Build a responseCard with a title, subtitle, and an optional set of options which should be displayed as buttons.
+    """
+    # buttons = None
+    # if options is not None:
+    #     buttons = []
+    #     for i in range(min(5, len(options))):
+    #         buttons.append(options[i])
+
+    return {
+            'title': title,
+            'subTitle': subtitle,
+            'imageUrl': img_url,
+            'attachmentLinkUrl': link_url,
+            # 'buttons': buttons,
+        }
+
+
+def close(session_attributes, fulfillment_state, message, response_cards):
+    response = {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'Close',
+            'fulfillmentState': fulfillment_state,
+            'message': {
+                'contentType': 'PlainText',
+                'content': message,
+            },
+            'responseCard': response_cards,
+        }
+    }
+
+    return response
 
 """ --- Helper Functions --- """
 
@@ -62,8 +100,13 @@ def search_restaurant(intent_request):
     response = requests.request("GET", url, headers=headers, params=querystring)
     # output_session_attributes = intent_request['sessionAttributes'] if intent_request['sessionAttributes'] is not None else {}
 
-    return ','.join([each['name'] for each in json.loads(response.text)['businesses'][:10]])
-
+    # return ','.join([each['name'] for each in json.loads(response.text)['businesses'][:10]])
+    response_cards = {
+        'contentType': 'application/vnd.amazonaws.card.generic',
+        # 'version': 1,
+        'genericAttachments': [build_response_card_attachment(each['name'], 'TBD', each['image_url'], each['url']) for each in json.loads(response.text)['businesses'][:10]]
+    }
+    return close({}, 'Fulfilled', 'Here\'re the choices you may consider:', response_cards)
 
 """ --- Intents --- """
 
@@ -79,44 +122,45 @@ def dispatch(intent_request):
 
     # Dispatch to your bot's intent handlers
     if intent_name == 'SearchRestaurant':
-        msg = search_restaurant(intent_request)
-        return {
-            'sessionAttributes': {},
-            'dialogAction': {
-                'type': 'Close',
-                'fulfillmentState': 'Fulfilled',
-                'message': {
-                    'contentType': 'PlainText',
-                    'content': msg,
-                },
-                'responseCard': {
-                    "version": 1,
-                    "contentType": "application/vnd.amazonaws.card.generic",
-                    "genericAttachments": [
-                        {
-                            "title": "What Flavor?",
-                            "subTitle": "What flavor do you want?",
-                            "imageUrl": "https://s3-media1.fl.yelpcdn.com/bphoto/H_vQ3ElMoQ8j1bKidrv_1w/o.jpg",
-                            "attachmentLinkUrl": "https://www.yelp.com/biz/molinari-delicatessen-san-francisco?adjust_creative=jwMDC4ZCoiDXl9qZtNl05w&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=jwMDC4ZCoiDXl9qZtNl05w",
-                            # "buttons": [
-                            #     {
-                            #         "text": "Lemon",
-                            #         "value": "lemon"
-                            #     },
-                            #     {
-                            #         "text": "Raspberry",
-                            #         "value": "raspberry"
-                            #     },
-                            #     {
-                            #         "text": "Plain",
-                            #         "value": "plain"
-                            #     }
-                            # ]
-                        }
-                    ]
-                }
-            }
-        }
+        return search_restaurant(intent_request)
+        # msg = search_restaurant(intent_request)
+        # return {
+        #     'sessionAttributes': {},
+        #     'dialogAction': {
+        #         'type': 'Close',
+        #         'fulfillmentState': 'Fulfilled',
+        #         'message': {
+        #             'contentType': 'PlainText',
+        #             'content': msg,
+        #         },
+        #         'responseCard': {
+        #             "version": 1,
+        #             "contentType": "application/vnd.amazonaws.card.generic",
+        #             "genericAttachments": [
+        #                 {
+        #                     "title": "What Flavor?",
+        #                     "subTitle": "What flavor do you want?",
+        #                     "imageUrl": "https://s3-media1.fl.yelpcdn.com/bphoto/H_vQ3ElMoQ8j1bKidrv_1w/o.jpg",
+        #                     "attachmentLinkUrl": "https://www.yelp.com/biz/molinari-delicatessen-san-francisco?adjust_creative=jwMDC4ZCoiDXl9qZtNl05w&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=jwMDC4ZCoiDXl9qZtNl05w",
+        #                     # "buttons": [
+        #                     #     {
+        #                     #         "text": "Lemon",
+        #                     #         "value": "lemon"
+        #                     #     },
+        #                     #     {
+        #                     #         "text": "Raspberry",
+        #                     #         "value": "raspberry"
+        #                     #     },
+        #                     #     {
+        #                     #         "text": "Plain",
+        #                     #         "value": "plain"
+        #                     #     }
+        #                     # ]
+        #                 }
+        #             ]
+        #         }
+        #     }
+        # }
 
     raise Exception('Intent with name ' + intent_name + ' not supported')
 
